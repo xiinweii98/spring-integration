91abf30bda851f3955a09f0f112ace6596cb57dd
Soh Xin Wei <xiinweii98@gmail.com>
2025-03-12 23:02:26 +0800

    chore: Initialise Gradle Spring Boot project

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # About
 
 This is a simple Spring Integration application to try out many of the functionalities it provide.
+
+## Project Dependencies
+
+Some of the dependencies used to build/run this application include:
+1. Gradle 8.12
+2. JDK 21.0.4
+
---
+++ spring-integration-app/src/main/java/com/differentiation/SpringIntegrationApplication.java
@@ -0,0 +1,16 @@
+package com.differentiation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point for the application.
+ *
+ * @author Soh Xin Wei
+ */
+@SpringBootApplication
+public class SpringIntegrationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntegrationApplication.class, args);
+    }
+}
